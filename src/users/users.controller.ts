cb131a949c2cafec654ebae09e8cc41401214784
StephenLeeDev <stephen.lee.dev@gmail.com>
2022-12-04 16:07:08 +0900

    Setup UserController

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,4 +1,28 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Get, Post, Req, Res } from '@nestjs/common';
 
 @Controller('users')
-export class UsersController {}
+export class UsersController {
+
+    @Get()
+    getUsers(@Req() req) {
+        return req.user;
+    }
+    
+    @Post()
+    postUsers(@Body() data: JoinRequestDto) {
+        
+    }
+    
+    @Post('login')
+    logIn(@Req() req) {
+        return req.user;
+    }
+    
+    @Post('logout')
+    logOut(@Req() req, @Res() res) {
+        req.logOut();
+        res.clearCookie('connect.sid', { httpOnly: true });
+        res.send('ok');
+    }
+
+}
